a9247f2ebead92844ee4b99d2598c08fcb240918
Eder Díaz <ederchrono@gmail.com>
2022-05-24 13:06:28 -0500

    add article with bad SEO

---
+++ pages/article.js
@@ -0,0 +1,60 @@
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Core Web Vitals
+        </h1>
+
+        <p>
+          Core Web Vitals are the subset of Web Vitals that apply to all web pages, should be measured 
+          by all site owners, and will be surfaced across all Google tools. Each of the Core Web Vitals 
+          represents a distinct facet of the user experience, is measurable in the field, and reflects 
+          the real-world experience of a critical user-centric outcome.
+        </p>
+
+        <p>
+          The metrics that make up Core Web Vitals will evolve over time. The current set for 2020 focuses 
+          on three aspects of the user experience—loading, interactivity, and visual stability—and includes 
+          the following metrics (and their respective thresholds):
+        </p>
+
+        <img src="https://web-dev.imgix.net/image/tcFciHGuF3MxnTr1y5ue01OGLBn2/ZZU8Z7TMKXmzZT2mCjJU.svg"></img>
+
+        <img src="https://web-dev.imgix.net/image/tcFciHGuF3MxnTr1y5ue01OGLBn2/iHYrrXKe4QRcb2uu8eV8.svg"></img>
+
+        <img src="https://web-dev.imgix.net/image/tcFciHGuF3MxnTr1y5ue01OGLBn2/dgpDFckbHwwOKdIGDa3N.svg"></img>
+
+        <ul>
+          <li>
+            <strong><a href="/lcp/">Largest Contentful Paint (LCP)</a></strong>: measures <em>loading</em> 
+            performance. To provide a good user experience, LCP should occur within <strong>2.5 seconds</strong> of 
+            when the page first starts loading.
+          </li>
+          <li>
+            <strong><a href="/fid/">First Input Delay (FID)</a></strong>: measures <em>interactivity</em>. 
+            To provide a good user experience, pages should have a FID of <strong>100 
+            milliseconds</strong> or less.
+          </li>
+          <li>
+            <strong><a href="/cls/">Cumulative Layout Shift (CLS)</a></strong>: measures 
+            <em>visual stability</em>. To provide a good user experience, pages should maintain a 
+            CLS of <strong>0.1.</strong> or less.
+          </li>
+        </ul>
+
+        <p>
+          For each of the above metrics, to ensure you're hitting the recommended target for most 
+          of your users, a good threshold to measure is the 75th percentile of page loads, segmented 
+          across mobile and desktop devices.
+        </p>
+
+        <p>This is a fragment of a <Link href="https://web.dev/i18n/en/vitals/">blog post</Link> by Philip Walton on web.dev</p>
+      </main>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -12,43 +13,19 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to News Site!
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <article className={styles.article}>
+          <Link href="/article" as="/article" passHref>
+            <a>
+              <h2>Article headline</h2>
+              <p>
+                posted on <time>{new Date().toLocaleDateString()}</time>
+              </p>
+            </a>
+          </Link>
+        </article>
       </main>
 
       <footer className={styles.footer}>
